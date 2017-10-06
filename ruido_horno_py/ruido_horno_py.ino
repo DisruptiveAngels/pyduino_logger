5c1eccbfc9ee771d84096c6097f6a9baf61f9587
baruc <baruc.almaguer@gmail.com>
2017-10-06 16:29:57 -0500

    Commit inicial

---
+++ ruido_horno_py/ruido_horno_py.ino
@@ -0,0 +1,61 @@
+#define LUZ 13
+#define SENS  A0
+#define DEF_DELAY 250
+
+int _led_state_ = LOW;
+int _horno_state_ = LOW;
+int _delay_ = DEF_DELAY;
+bool _running_ = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  pinMode(LUZ, OUTPUT);
+  pinMode(A0, INPUT);
+  digitalWrite(LUZ, _led_state_);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial.available()){
+    char data = Serial.read();
+    switch(data){
+      case 'L':
+        _led_state_ = HIGH;
+        digitalWrite(LUZ, _led_state_);
+        break;
+      case 'l':
+        _led_state_ = LOW;
+        digitalWrite(LUZ, _led_state_);
+        break;
+      case 'H':
+        _horno_state_ = HIGH;
+        break;
+      case 'h':
+        _horno_state_ = LOW;
+        break;
+      case '+':
+        _delay_ = _delay_ > 50 ? _delay_- 10 : _delay_;
+        break;
+      case '-':
+        _delay_ = _delay_ < 1000 ? _delay_+ 10 : _delay_;
+        break;
+      case 'S':
+        _running_ = true;
+        break;
+      case 's':
+        _running_ = false;
+        break;
+    }
+  }
+  if(_running_){
+    Serial.print((float)(millis())/1000.0);
+    Serial.print("\t");
+    Serial.print(_delay_);
+    Serial.print("\t");
+    Serial.print(_led_state_ ? "L1\t" : "L0\t");
+    Serial.print(_horno_state_ ? "H1\t" : "H0\t");
+    Serial.println(analogRead(A0));   
+    delay(_delay_);
+  }
+}
---
+++ ruido_winlin.py
@@ -0,0 +1,160 @@
+import os
+import sys, argparse
+import time, datetime
+import csv
+
+import serial
+import serial.tools.list_ports as listPorts
+
+#la magia del teclado en unix y windows
+import kb_handler
+
+_menu_ = """========================
+Commands:
+[L/l] LED on/off
+[H/h] Horno on/off
+[+]   Muestreo - 10ms
+[-]   Muestreo + 10ms
+[S/s] Start/Pause test
+[ESC] Finish test and exit
+========================"""
+
+_clearcmd_ = "cls" if os.name == "nt" else "clear"
+
+result_fname = "out.csv"
+_running_ = False
+_sampling_ = 250
+_led_ = False
+_horno_ = False
+_sensor_ = 0
+
+#windows only
+def arduinoSerial():
+	portname = ""
+	for port in listPorts.comports():
+		if("Arduino" in port[1]):
+			portname = port[0]
+			break
+	return portname
+
+def printMenu(cmds):
+	global _clearcmd_
+	os.system(_clearcmd_)
+	if cmds:
+		print(_menu_)
+	printResults(result_fname,_running_, _sampling_, _led_, _horno_, _sensor_)
+		
+def printResults(file,state, samples, led, horno, sens):
+	print("out=>\t\t{0}".format(file))
+	print("Estado=>\t{0}".format("Running" if state else "Paused"))
+	print("Muestreo=>\t{0}".format(samples))
+	print("LED=>\t\t{0}".format("ON" if led else "OFF"))
+	print("Horno=>\t\t{0}".format("ON" if horno else "OFF"))
+	print("Sensor=>\t{0}".format(sens))
+
+def main(argv):
+	global result_fname
+	global _running_
+	global _sampling_
+	global _led_
+	global _horno_
+	global _sensor_
+	
+	pr_cmds = False
+	#Parser de las opciones de linea de comando
+	parser = argparse.ArgumentParser()
+	#opciones
+	parser.add_argument("-o", "--output", help="name of the file storing the results (def=out.csv)")
+	parser.add_argument("-c", "--commands",action="store_true", help="print usable commands above results")
+	args = parser.parse_args()
+	#procesa opciones
+	if args.output:
+		result_fname = args.output
+	if args.commands:
+		pr_cmds = True
+	
+	#setup stuff
+	kb = kb_handler.KBHit()
+	ser = serial.Serial()
+	baudrate = 19200
+	portname = arduinoSerial()
+	if(not portname == ""):
+		try:
+			ser = serial.Serial(portname, baudrate, timeout=2)
+			
+			#Puerto serial abierto correctamente, control del arduino:
+			with open(result_fname, "a") as ofile:
+				ofile.write("Time\tdelay\tLED\tHorno\tSensor" + "\tdate: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + '\n')
+				notdone = True
+				while notdone:
+					printMenu(pr_cmds)
+					#control
+					while kb.kbhit():
+						k = kb.getch()
+						if k in ['L', 'l']:
+							_led_ = not _led_
+							ser.write(b'L' if _led_ == True else b'l')
+						elif k in ['H', 'h']:
+							_horno_ = not _horno_
+							ser.write(b'H' if _horno_ == True else b'h')
+						elif k == '+':
+							_sampling_ = _sampling_ - 10 if _sampling_ > 20 else _sampling_
+							ser.write(b'+')
+						elif k == '-':
+							_sampling_ = _sampling_ + 10 if _sampling_ < 1000 else _sampling_
+							ser.write(b'-')
+						elif k in ['S', 's']:
+							_running_ = not _running_
+							ser.write(b'S' if _running_ == True else b's')
+						elif ord(k) == 27:
+							if _running_:
+								ser.write(b's') #stop comm
+							if ser.isOpen():
+								ser.close()
+							print("Nighty night!!")
+							notdone = False
+							break
+					if not notdone: #doble negacion
+						break
+						
+					#reception de datos seriales
+					
+					datareceived = False
+					serline = ""
+					#espera a tener al menos una linea completa
+					
+					while ser.inWaiting():
+						serline = ser.readline().decode("utf-8").rstrip()
+						ofile.write(serline + '\n')
+						datareceived = True
+					ofile.flush()
+					
+					#obtiene dato de sensor
+					if datareceived:
+						cols = serline.split('\t')
+						try:
+							_sensor_ = int(cols[-1])
+						except:
+							_sensor_ = "Error parsing sensor data"
+						#print (cols)
+					
+					
+					#print to file the header:
+					#  Serial.println("Time\tdel\tLd\tHn\tVal");
+					
+					time.sleep(0.1) #30FPS no matter what
+				
+		except Exception as error:
+			#print("Error opening serial port: {0}".format(portname))
+			#if(ser is not None and ser.isOpen()):
+			#	ser.close()
+			
+			print('caught this error: ' + repr(error))
+			exit()
+	else:
+		print("Arduino not found")
+		exit()
+
+if __name__ == "__main__" :
+	main(sys.argv)
+	
